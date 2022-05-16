5508148f9809c5487d49e7e9771f283eb77b3933
wagner <wagner30023@gmail.com>
2022-05-16 07:41:03 -0300

    Configurando e criando a API

---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    /**
+     * @var product
+     */
+
+    private $product;
+
+    public function __construct(Product $product)
+    {
+        $this->product = $product;
+    }
+
+    public function index()
+    {
+        $products = $this->product->all();
+        return response()->json($products);
+    }
+
+    public function show($id)
+    {
+        $products = $this->product->find($id);
+        return response()->json($products);
+    }
+
+    public function save(Request $request)
+    {
+        $data = $request->all();
+        $product = $this->product->create($data);
+        return response()->json($product);
+    }
+
+    public function update(Request $request)
+    {
+        $data = $request->all();
+        $product = $this->product->find($data['id']);
+
+        $product->update($data);
+
+        return response()->json($product);
+    }
+
+    public function delete($id)
+    {
+        $product = $this->product->find($id);
+        $product->delete();
+
+        return response()->json(['data' => ['msg' => 'Produto foi removido com sucesso!']]);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ProductController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+// Route::get('/teste',function(){
+//     // dd($request->headers->get('Authorization'));
+//     $response = new \Illuminate\Http\Response(json_encode(['msg' => 'minha primeira resposta de API']));
+//     $response->headers->set('Content-Type', 'application/json');
+//     return $response;
+// });
+
+
+Route::namespace('Api')->prefix('products')->group(function(){
+    Route::get('/',[ProductController::class,'index']);
+    Route::get('/{id}',[ProductController::class,'show']);
+    Route::post('/',[ProductController::class,'save']);
+    Route::put('/',[ProductController::class,'update']);
+    Route::delete('/{id}',[ProductController::class,'delete']);
+});
